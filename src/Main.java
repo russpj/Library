eefc074124a9e4404dc78ba9b1edf5e26138139d
Russ Paul-Jones <russ@paul-jones.org>
2017-12-05 20:13:26 -0800

    Building an array of BookTransactions

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		ArrayList<Event> events = new ArrayList<Event>();
 		
+		System.out.println("Using hand-rolled Events");
 		events.add(new Event("Head First Java", 1, Event.DeskEvent.checkout));
 		events.add(new Event("Head First Java", 1, Event.DeskEvent.checkout));
 		events.add(new Event("Head First Java", 2, Event.DeskEvent.checkout));
@@ -22,7 +23,18 @@ public class Main {
 		
 		System.out.print("There were at most ");
 		System.out.print(maxCheckedOut);
+		System.out.println(" books checked out"); 
 		
-		System.out.println(" books checked out");  
+		System.out.println("Using Book Transactions");
+		
+		ArrayList<BookTransaction> trans = new ArrayList<BookTransaction>();
+		
+		trans.add(new BookTransaction("Head First Java", 1, 7));
+		trans.add(new BookTransaction("Head First Java", 1, 3));
+		trans.add(new BookTransaction("Head First Java", 2, 3));
+		trans.add(new BookTransaction("Head First Java", 4, -1));
+		trans.add(new BookTransaction("Head First Java", 5, 10));
+		trans.add(new BookTransaction("Head First Java", 8, 9));
+		trans.add(new BookTransaction("Head First Java", 8, -1));
 	}
 }
