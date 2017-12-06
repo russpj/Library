0fefe36ef42b61c1cc166d821e66552ed7e7a41a
Russ Paul-Jones <russ@paul-jones.org>
2017-12-05 20:05:00 -0800

    Added BookTransaction class

---
+++ src/BookTransaction.java
@@ -0,0 +1,16 @@
+
+public class BookTransaction {
+	BookTransaction(String nameIn, int dayOutIn, int dayInIn)
+	{
+		name = nameIn;
+		dayOut = dayOutIn;
+		dayIn = dayInIn;
+	}
+	
+	
+	
+	public String name;
+	public int dayOut;
+	public int dayIn;
+
+}
