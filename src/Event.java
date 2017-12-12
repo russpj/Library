de386bdb01c5989d5522e7bfefc33d1c2652a051
Russ Paul-Jones <russ@paul-jones.org>
2017-12-12 11:35:34 -0800

    Refactor - Moved EventOrder class into Event class as Order

--- src/Event.java
+++ src/Event.java
@@ -46,6 +46,19 @@ public class Event {
 		return maxCount;
 	}
 	
+	private static class Order implements Comparator<Event>
+	{
+		public int compare(Event event1, Event event2)
+		{
+			if (event1.day != event2.day)
+				return event1.day - event2.day;
+			else if (event1.stamp != event2.stamp)
+				return (event1.stamp == Event.Stamp.checkin) ? -1 : 1;
+			else
+				return (event1.bookName.compareTo(event2.bookName));
+		}
+	}
+	
 	public static List<Event> EventsFromTrans(List<Slip> slips)
 	{
 		ArrayList<Event> events = new ArrayList<Event>();
@@ -63,7 +76,7 @@ public class Event {
 			}
 		}
 		
-		events.sort(new EventOrder());
+		events.sort(new Order());
 		
 		return events;
 	}
